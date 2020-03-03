de49361fed8e09379113a41d85158136326cfe57
Novixous <anonymousvhb@gmail.com>
2020-03-03 23:12:43 +0700

    Initialize Server Code

---
+++ Task-Management-Server/src/main/java/com/task/management/server/taskmanagementserver/TaskManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.task.management.server.taskmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementServerApplication.class, args);
+    }
+
+}
---
+++ Task-Management-Server/src/test/java/com/task/management/server/taskmanagementserver/TaskManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.management.server.taskmanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
